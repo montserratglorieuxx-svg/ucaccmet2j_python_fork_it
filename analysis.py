4ca6e040fd7adcbaa07c76b650967051f2c1aed9
Montserrat <montserratglorieuxx@gmail.com>
2026-01-13 12:07:54 +0100

    get seattle total monthly precipitation

---
+++ analysis.py
@@ -0,0 +1,30 @@
+import json
+with open('precipitation.json') as file:
+    data = json.load(file)
+
+seattle=[]
+for measurement in data: 
+    if measurement["station"]=="GHCND:US1WAKG0038":
+        seattle.append(measurement)
+
+total_monthly_precipitation = {}
+for measurement in seattle:
+    split_date=measurement["date"].split("-")
+    measurement["date"]=split_date[1]
+
+    month = measurement['date']
+    if month not in total_monthly_precipitation:
+        total_monthly_precipitation[month] = 0
+    total_monthly_precipitation[month] += measurement['value']
+
+print(total_monthly_precipitation)
+
+with open('results.json', 'w', encoding='utf-8') as file:
+    json.dump(total_monthly_precipitation, file, indent=4) 
+
+
+
+
+# months={}
+# for measurement in seattle:
+#     month = measurement[]
