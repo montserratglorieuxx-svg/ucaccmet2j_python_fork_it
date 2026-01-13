dda8f2bbc553fd02cbf99d0e21dc3109a6ebb125
Montserrat <montserratglorieuxx@gmail.com>
2026-01-13 14:11:00 +0100

    fix bug with seattle

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ for measurement in data:
     if measurement["station"]=="GHCND:US1WAKG0038":
         seattle.append(measurement)
 
+
 total_monthly_precipitation = {}
 for measurement in seattle:
     split_date=measurement["date"].split("-")
@@ -17,6 +18,8 @@ for measurement in seattle:
         total_monthly_precipitation[month] = 0
     total_monthly_precipitation[month] += measurement['value']
 
+    measurement["date"]=split_date
+
 print(total_monthly_precipitation)
 
 with open('results.json', 'w', encoding='utf-8') as file:
@@ -25,6 +28,8 @@ with open('results.json', 'w', encoding='utf-8') as file:
 
 
 
-# months={}
-# for measurement in seattle:
-#     month = measurement[]
+
+# x=0
+# total_yearly_precipitation = {}
+# for month in total_monthly_precipitation:
+#     x=x+month['value']
